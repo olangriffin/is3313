e12e95248d7869b18e8ae24aef6aa298e3c58dc0
Olan Griffin <olangriffin1@gmail.com>
2024-11-08 11:23:55 +0000

    second commit

---
+++ mymath.py
@@ -0,0 +1,19 @@
+def add_three(a, b, c):
+    return a + b + c
+
+def subtract_three(a, b, c):
+    return a - b - c
+
+def multiply_three(a, b, c):
+    return a * b * c
+
+def divide_three(a, b, c):
+    if b == 0 or c == 0:
+        raise ValueError("Cannot divide by zero")
+    return (a / b) / c
+
+def square(a):
+    return a ** 2
+
+def cube(a):
+    return a ** 3
